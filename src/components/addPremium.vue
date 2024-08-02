991c6ebe8c09382f88681638830ebbb2e3faa3f0
Kanat <abdsattar02@inbox.ru>
2024-08-02 14:25:30 +0500

    fix(add premium)

--- src/components/addPremium.vue
+++ src/components/addPremium.vue
@@ -3,7 +3,7 @@ import crown from './icons/crown.vue';
 import { PhCaretRight } from "@phosphor-icons/vue";
 </script>
 <template>
-    <div class="rounded-2xl p-px bg-gradient-to-r from-zinc-700 from-0% via-black via-50% to-zinc-700 to-100%">
+    <div class="rounded-2xl p-px bg-gradient-to-r from-black from-0% via-black via-95% to-[#FFE500] to-100%">
         <div class="rounded-[calc(1.5rem-10px)] p-1 bg-[#17181C] py-4 flex justify-between">
             <div>
                 <crown />
