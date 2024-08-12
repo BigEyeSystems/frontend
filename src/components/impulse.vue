3600ad7787e84f77a0310f26c2439fb2da83994d
baibatyr_k <kabdsattar@inbox.ru>
2024-08-12 08:48:33 +0500

    fix(impulse buttons)

--- src/components/impulse.vue
+++ src/components/impulse.vue
@@ -2,38 +2,72 @@
 import ButtonView from "./button.vue";
 import ticker from "./ticker.vue";
 import { ref } from "vue";
+
+const selectedInterval = ref(null);
+const selectedHistory = ref(null);
 const showImpulse = ref(false);
+
 const toggleImpulse = () => {
   showImpulse.value = !showImpulse.value;
 };
+
+const selectInterval = (index) => {
+  selectedInterval.value = index;
+};
+
+const selectHistory = (index) => {
+  selectedHistory.value = index;
+};
 </script>
+
 <template>
   <div class="text-xs">
     <div v-if="!showImpulse">
       <div class="mb-3">
         <p>Выберете временной интервал (мин)</p>
         <div class="flex gap-2 mt-3">
-          <button class="bg-[#17181C] w-full py-2 rounded">1</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">5</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">15</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">60</button>
+          <button
+            v-for="(interval, index) in [1, 5, 15, 60]"
+            :key="index"
+            :class="{
+              'bg-[#7064F3]': selectedInterval === index,
+              'bg-[#17181C]': selectedInterval !== index
+            }"
+            @click="selectInterval(index)"
+            class="w-full py-2 rounded"
+          >
+            {{ interval }}
+          </button>
         </div>
       </div>
       <div>
         <p>История</p>
         <input class="w-full my-3 p-2" type="text" />
         <div class="flex gap-2 my-3">
-          <button class="bg-[#17181C] w-full py-2 rounded">5%</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">10%</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">15%</button>
-          <button class="bg-[#17181C] w-full py-2 rounded">20%</button>
+          <button
+            v-for="(history, index) in [5, 10, 15, 20]"
+            :key="index"
+            :class="{
+              'bg-[#7064F3]': selectedHistory === index,
+              'bg-[#17181C]': selectedHistory !== index
+            }"
+            @click="selectHistory(index)"
+            class="w-full py-2 rounded"
+          >
+            {{ history }}%
+          </button>
         </div>
       </div>
     </div>
 
-    <ButtonView v-if="!showImpulse" :text="'Получить информацию'" :on-click="toggleImpulse" class="mt-4"/>
+    <ButtonView
+      v-if="!showImpulse"
+      :text="'Получить информацию'"
+      :on-click="toggleImpulse"
+      class="mt-4"
+    />
     <div v-else>
-      <ButtonView  :text="'Добавить информацию'" class="my-3" />
+      <ButtonView :text="'Добавить информацию'" class="my-3" />
     </div>
 
     <div v-if="showImpulse">
