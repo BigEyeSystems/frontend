d914ab92dc03fd7faa484c220fb1eb849211890d
Kanat <abdsattar02@inbox.ru>
2024-08-24 20:52:52 +0500

    fix(login api token)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -80,6 +80,7 @@ onBeforeUnmount(() => {
 <template>
   <div class="flex flex-col min-h-screen">
     <Header :UserName="user?.username" :lang="'RU'"/>
+    {{ localStorage.getItem('token') }}
     <div class="flex-grow z-1 py-4">
       <div class="flex gap-4 my-4">
         <menuItems title="Импульсы цены" @click="toggleTeleport">
