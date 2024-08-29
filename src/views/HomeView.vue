429fae9e3cd2d7946e5956020970db9c53a8867c
Kanat <abdsattar02@inbox.ru>
2024-08-29 12:55:00 +0500

    fix(top 5 ticker date)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -133,7 +133,7 @@ onBeforeUnmount(() => {
         <p class="text-xs">Последнее обновление:</p>
         <div class="flex text-xs gap-1">
           <PhClock :size="12" /> 12:03
-          <PhCalendarDots :size="16" /> {{data.top_tickers_by_volume.last_update_time}}
+          <PhCalendarDots :size="16" /> {{data?.top_tickers_by_volume?.last_update_time}}
         </div>
       </div>
       <div class="my-4 mb-20">
