9cd0abc55986489b44ccbf5015681ab4d806497f
baibatyr_k <kabdsattar@inbox.ru>
2024-08-30 14:48:12 +0500

    fix(style)

--- src/components/gradation.vue
+++ src/components/gradation.vue
@@ -86,7 +86,7 @@ const toggleGradation = async () => {
                     <PhCalendarDots :size="16" /> 9.01.2024
                 </div>
             </div>
-            <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items center">
+            <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center">
                 <div class="flex gap-3 items-center">
                     <div class="p-2 bg-[#797979]">
                         <PhFile :size="24" color="#fff"/>
