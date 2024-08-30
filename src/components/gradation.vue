ad50722cb08c9ebb988cb1ff87d687b9687b8c37
baibatyr_k <kabdsattar@inbox.ru>
2024-08-30 09:23:43 +0500

    fix(file export)

--- src/components/gradation.vue
+++ src/components/gradation.vue
@@ -37,8 +37,9 @@ const downloadGradationGrowthFile = async (id) => {
             }
         );
         downloadData.value = response.data;
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement("a");
+        const blob = new Blob([response.data], {type: 'text/csv; charset=utf-8'});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
         link.href = url;
         link.setAttribute("download", "gradation_growth.csv");
         document.body.appendChild(link);
