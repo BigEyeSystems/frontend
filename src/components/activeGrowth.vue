c4bcd77f4f2e377a499e26d4bad798c014c286e1
Kanat <abdsattar02@inbox.ru>
2024-09-02 16:40:50 +0500

    feature(active growth)

--- src/components/activeGrowth.vue
+++ src/components/activeGrowth.vue
@@ -2,14 +2,59 @@
 import ButtonView from "./button.vue";
 import { ref } from "vue";
 
+const fileInterval = ref(30);
 const selectedInterval = ref(null);
-const selectInterval = (index) => {
+const selectInterval = (index, interval) => {
   selectedInterval.value = index;
+  fileInterval.value = interval;
 };
-
+const gradationActiveData = ref(null); 
+const historyData = ref(null);
 const showGradationGrowth = ref(false);
-const toggleGradationGrowth = () => {
-    showGradationGrowth.value = !showGradationGrowth.value;
+const toggleGradationGrowth = async () => {
+    try {
+        const response = await axios.get(
+            `https://dsde1736.fornex.org/api/data/gradation_growth?interval=${fileInterval.value}&growth_type=Price`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
+        gradationActiveData.value = response.data;
+        showGradationGrowth.value = true;
+
+    } catch(error) {
+        console.log('Error fetching data: ' + error );
+    }
+    try {
+        const responseHistory = await axios.get(
+            "https://dsde1736.fornex.org/api/data/gradation_growth_history?growth_type=Price",
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
+        historyData.value = responseHistory.data;
+    } catch(error) {
+        console.log('Error fetching data: ' + error );
+    }
+};
+const downloadGradationGrowthFile = async (id) => {
+    try {
+        const response = await axios.get(
+            `https://dsde1736.fornex.org/api/webhook/send_growth_data?file_id=${id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+                responseType: 'text',
+            }
+        );
+    } catch(error) {
+        console.log('Error downloading data: ' + error );
+    }
 };
 </script>
 <template>
@@ -21,7 +66,7 @@ const toggleGradationGrowth = () => {
                     <button v-for="(interval, index) in [5, 30, 60, 4]" :key="index" :class="{
                         'bg-[#92FBDB] text-black font-semibold': selectedInterval === index,
                         'bg-[#17181C]': selectedInterval !== index
-                    }" @click="selectInterval(index)" class="w-full py-2 rounded">
+                    }" @click="selectInterval(index, interval)" class="w-full py-2 rounded">
                         <p v-if="interval !== 4">{{ interval }} мин</p>
                         <p v-else>{{ interval }} часа </p>
                     </button>
@@ -40,6 +85,45 @@ const toggleGradationGrowth = () => {
                     <PhClock :size="16" /> 12:03
                     <PhCalendarDots :size="16" /> 9.01.2024
                 </div>
+            </div>
+            <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center" @click="downloadGradationGrowthFile(gradationActiveData?.file_id)">
+                <div class="flex gap-3 items-center">
+                    <div class="p-1 bg-[#797979] rounded">
+                        <PhFile :size="24" color="#fff"/>
+                    </div>
+                    <p class="text-sm font-semibold">
+                      {{ gradationActiveData?.file_name }}
+                    </p>
+                </div>
+                <PhDownloadSimple :size="24" />
+            </div>
+            <p class="mb-3 text-sm font-semibold">История</p>
+            <div v-if="historyData">
+                <div v-for="(file,index) in historyData.data" key="index">
+                    <div class="flex justify-between">
+                        <p class="text-[#B8B8B8] text-xs">Дата создания</p>
+                        <div class="flex gap-1 text-xs items-center">
+                            <PhClock :size="12" />
+                            <p>{{ file.time }}</p>
+                            <PhCalendarDots :size="16" />
+                            <p>{{ file.date }}</p>
+                        </div>
+                    </div>
+                    <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center" @click="downloadGradationGrowthFile(file.file_id)">
+                        <div class="flex gap-3 items-center">
+                            <div class="p-1 bg-[#797979] rounded">
+                                <PhFile :size="24" color="#fff"/>
+                            </div>
+                            <p class="text-sm font-semibold">
+                                {{ file.file_name }}
+                            </p>
+                        </div>
+                        <PhDownloadSimple :size="24" />
+                    </div>
+                </div>
+            </div>
+            <div v-else>
+                No data
             </div>
         </div>
     </div>
