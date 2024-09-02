64cb8accd39ef9ece2387b76aedfaab7aaeea152
Kanat <abdsattar02@inbox.ru>
2024-09-02 14:53:59 +0500

    fix(funding history data date)

--- src/components/fundingData.vue
+++ src/components/fundingData.vue
@@ -101,41 +101,28 @@ const toggleFundingData = async () => {
 </script>
 <template>
   <div class="text-xs">
-    <ButtonView
-      v-if="!showFundingData"
-      :text="'Получить информацию'"
-      @click="toggleFundingData"
-      class="my-3"
-    />
+    <ButtonView v-if="!showFundingData" :text="'Получить информацию'" @click="toggleFundingData" class="my-3" />
     <ButtonView v-else :text="'Обновить информацию'" class="my-3" />
     <div v-if="showFundingData">
       <p class="mb-3">Результат запроса</p>
       <div class="flex justify-between">
         <p class="text-xs">Последнее обновление:</p>
         <div class="flex text-xs gap-1">
-          <PhClock :size="16" /> 12:03 <PhCalendarDots :size="16" /> 9.01.2024
+          <PhClock :size="16" /> 12:03
+          <PhCalendarDots :size="16" /> 9.01.2024
         </div>
       </div>
       <div class="flex gap-2 mt-4">
-        <button
-          class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
-          :class="[dayData ? 'bg-[#92FBDB] text-black font-semibold' : '']"
-          @click="showDailyData"
-        >
+        <button class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
+          :class="[dayData ? 'bg-[#92FBDB] text-black font-semibold' : '']" @click="showDailyData">
           За день
         </button>
-        <button
-          class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
-          :class="[weekData ? 'bg-[#92FBDB] text-black font-semibold' : '']"
-          @click="showWeeklyData"
-        >
+        <button class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
+          :class="[weekData ? 'bg-[#92FBDB] text-black font-semibold' : '']" @click="showWeeklyData">
           За неделю
         </button>
-        <button
-          class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
-          :class="[monthData ? 'bg-[#92FBDB] text-black font-semibold' : '']"
-          @click="showMonthlyData"
-        >
+        <button class="bg-[#17181C] p-2 w-full text-xs text-[#B8B8B8] rounded"
+          :class="[monthData ? 'bg-[#92FBDB] text-black font-semibold' : '']" @click="showMonthlyData">
           За месяц
         </button>
       </div>
@@ -144,36 +131,32 @@ const toggleFundingData = async () => {
       </div>
       <div class="flex flex-col gap-2">
         <div class="flex gap-2 items-center">
-          <div
-            class="px-3 bg-[#33A721] text-sm font-semibold h-4 flex items-center"
-          >
+          <div class="px-3 bg-[#33A721] text-sm font-semibold h-4 flex items-center">
             {{ fundingData?.positive_quantity }}
           </div>
           <p class="text-xs text-[#B8B8B8]">Кол-во положительных фандингов</p>
         </div>
         <div class="flex gap-2 items-center">
-          <p
-            class="px-3 bg-[#CA3140] text-sm font-semibold h-4 flex items-center"
-          >
+          <p class="px-3 bg-[#CA3140] text-sm font-semibold h-4 flex items-center">
             {{ fundingData?.negative_quantity }}
           </p>
           <p class="text-xs text-[#B8B8B8]">Кол-во отрицательных фандингов</p>
         </div>
         <div class="flex gap-2 items-center">
-          <p
-            class="px-3 bg-white text-sm font-semibold h-4 flex items-center text-black"
-          >
+          <p class="px-3 bg-white text-sm font-semibold h-4 flex items-center text-black">
             {{ fundingData?.neutral_quantity }}
           </p>
           <p class="text-xs text-[#B8B8B8]">Кол-во нейтральных фандингов</p>
         </div>
       </div>
-      <p class="mb-3 text-sm font-semibold">История</p>
-      <p class="text-[#B8B8B8] text-xs">Дата создания</p>
+      <p class="my-4 text-sm font-semibold">История</p>
       <div v-for="(data, index) in historyData.data" :key="index">
-        <p>Кол-во положительных фандингов: {{ data.positive_count }}</p>
-        <p>Кол-во отрицательных фандингов: {{ data.negative_count }}</p>
-        <p>Кол-во нейтральных фандингов: {{ data.neutral_count }}</p>
+        <div class="my-4">
+          <p class="text-[#B8B8B8] text-xs">Дата создания</p>
+          <p>Кол-во положительных фандингов: {{ data.positive_count }}</p>
+          <p>Кол-во отрицательных фандингов: {{ data.negative_count }}</p>
+          <p>Кол-во нейтральных фандингов: {{ data.neutral_count }}</p>
+        </div>
       </div>
     </div>
   </div>
