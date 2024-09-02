39fdedc2816f4aa53ee86b54afee9738c491b68f
Kanat <abdsattar02@inbox.ru>
2024-09-02 10:14:33 +0500

    fix(history date)

--- src/components/gradation.vue
+++ src/components/gradation.vue
@@ -101,8 +101,9 @@ const downloadGradationGrowthFile = async (id) => {
             <p class="mb-3 text-sm font-semibold">История</p>
             <div v-if="historyData">
                 <div v-for="(file,index) in historyData.data" key="index">
-                    <div>
-                        <p>Дата создания</p>
+                    <div class="flex justify-between">
+                        <p class="text-[#B8B8B8] text-sm">Дата создания</p>
+                        {{ file }}
                         <p>{{ file.date }}</p>
                     </div>
                     <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center" @click="downloadGradationGrowthFile(file.file_id)">
