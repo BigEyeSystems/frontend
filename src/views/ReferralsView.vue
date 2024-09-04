54dc4a8ed95b0653cfddb4b2e235afa945cc651e
baibatyr_k <kabdsattar@inbox.ru>
2024-09-04 09:17:23 +0500

    fix(referral notification)

--- src/views/ReferralsView.vue
+++ src/views/ReferralsView.vue
@@ -171,10 +171,15 @@ const copyReferral = async () => {
       <transition name="modal">
         <div
           v-if="isNotification"
-          class="modal fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
+          class="modal fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 px-4"
         >
-          <div class="bg-white p-4 rounded-lg">
-            <p>Ссылка скопирована в буфер обмена</p>
+          <div class="bg-[##222222b3] p-4 rounded-lg">
+            <div class="flex justify-end">
+              <button @click="isNotification = false">
+                <PhX :size="21" />
+              </button>
+            </div>
+            <p class="text-lg">Ссылка скопирована в буфер обмена</p>
           </div>
         </div>
       </transition>
