29e883606750124d24a76eb57d3fbf9f47d6318d
baibatyr_k <kabdsattar@inbox.ru>
2024-09-04 12:45:12 +0500

    fix(file notification active)

--- src/components/activeGrowth.vue
+++ src/components/activeGrowth.vue
@@ -9,123 +9,161 @@ const selectInterval = (index, interval) => {
   selectedInterval.value = index;
   fileInterval.value = interval;
 };
-const gradationActiveData = ref(null); 
+const gradationActiveData = ref(null);
 const historyData = ref(null);
 const showGradationGrowth = ref(false);
-const toggleGradationGrowth = async () => {
-    try {
-        const response = await axios.get(
-            `https://dsde1736.fornex.org/api/data/gradation_growth?interval=${fileInterval.value}&growth_type=Price`,
-            {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-            }
-        );
-        gradationActiveData.value = response.data;
-        showGradationGrowth.value = true;
+const isNotification = ref(false);
 
-    } catch(error) {
-        console.log('Error fetching data: ' + error );
-    }
-    try {
-        const responseHistory = await axios.get(
-            "https://dsde1736.fornex.org/api/data/gradation_growth_history?growth_type=Price",
-            {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-            }
-        );
-        historyData.value = responseHistory.data;
-    } catch(error) {
-        console.log('Error fetching data: ' + error );
-    }
+const toggleGradationGrowth = async () => {
+  try {
+    const response = await axios.get(
+      `https://dsde1736.fornex.org/api/data/gradation_growth?interval=${fileInterval.value}&growth_type=Price`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    gradationActiveData.value = response.data;
+    showGradationGrowth.value = true;
+  } catch (error) {
+    console.log("Error fetching data: " + error);
+  }
+  try {
+    const responseHistory = await axios.get(
+      "https://dsde1736.fornex.org/api/data/gradation_growth_history?growth_type=Price",
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    historyData.value = responseHistory.data;
+  } catch (error) {
+    console.log("Error fetching data: " + error);
+  }
 };
 const downloadGradationGrowthFile = async (id) => {
-    try {
-        const response = await axios.get(
-            `https://dsde1736.fornex.org/api/webhook/send_growth_data?file_id=${id}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-                responseType: 'text',
-            }
-        );
-    } catch(error) {
-        console.log('Error downloading data: ' + error );
-    }
+  try {
+    const response = await axios.get(
+      `https://dsde1736.fornex.org/api/webhook/send_growth_data?file_id=${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        responseType: "text",
+      }
+    );
+    isNotification.value = true;
+  } catch (error) {
+    console.log("Error downloading data: " + error);
+  }
 };
 </script>
 <template>
-    <div class="text-xs">
-        <div v-if="!showGradationGrowth">
-            <div class="mb-3">
-                <p>Выберите интервал</p>
-                <div class="flex gap-2 mt-3">
-                    <button v-for="(interval, index) in [5, 30, 60, 4]" :key="index" :class="{
-                        'bg-[#92FBDB] text-black font-semibold': selectedInterval === index,
-                        'bg-[#17181C]': selectedInterval !== index
-                    }" @click="selectInterval(index, interval)" class="w-full py-2 rounded">
-                        <p v-if="interval !== 4">{{ interval }} мин</p>
-                        <p v-else>{{ interval }} часа </p>
-                    </button>
-                </div>
-            </div>
+  <div class="text-xs">
+    <div v-if="!showGradationGrowth">
+      <div class="mb-3">
+        <p>Выберите интервал</p>
+        <div class="flex gap-2 mt-3">
+          <button
+            v-for="(interval, index) in [5, 30, 60, 4]"
+            :key="index"
+            :class="{
+              'bg-[#92FBDB] text-black font-semibold':
+                selectedInterval === index,
+              'bg-[#17181C]': selectedInterval !== index,
+            }"
+            @click="selectInterval(index, interval)"
+            class="w-full py-2 rounded"
+          >
+            <p v-if="interval !== 4">{{ interval }} мин</p>
+            <p v-else>{{ interval }} часа</p>
+          </button>
         </div>
-        <ButtonView v-if="!showGradationGrowth" :text="'Получить информацию'" :on-click="toggleGradationGrowth"
-            class="my-3" />
-        <ButtonView v-else :text="'Обновить информацию'" class="my-3" />
-
-        <div v-if="showGradationGrowth">
-            <p class="mb-3">Результат запроса</p>
-            <div class="flex justify-between">
-                <p class="text-xs">Последнее обновление:</p>
-                <div class="flex text-xs gap-1">
-                    <PhClock :size="16" /> 12:03
-                    <PhCalendarDots :size="16" /> 9.01.2024
-                </div>
-            </div>
-            <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center" @click="downloadGradationGrowthFile(gradationActiveData?.file_id)">
-                <div class="flex gap-3 items-center">
-                    <div class="p-1 bg-[#797979] rounded">
-                        <PhFile :size="24" color="#fff"/>
-                    </div>
-                    <p class="text-sm font-semibold">
-                      {{ gradationActiveData?.file_name }}
-                    </p>
-                </div>
-                <PhDownloadSimple :size="24" />
-            </div>
-            <p class="mb-3 text-sm font-semibold">История</p>
-            <div v-if="historyData">
-                <div v-for="(file,index) in historyData.data" key="index">
-                    <div class="flex justify-between">
-                        <p class="text-[#B8B8B8] text-xs">Дата создания</p>
-                        <div class="flex gap-1 text-xs items-center">
-                            <PhClock :size="12" />
-                            <p>{{ file.time }}</p>
-                            <PhCalendarDots :size="16" />
-                            <p>{{ file.date }}</p>
-                        </div>
-                    </div>
-                    <div class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center" @click="downloadGradationGrowthFile(file.file_id)">
-                        <div class="flex gap-3 items-center">
-                            <div class="p-1 bg-[#797979] rounded">
-                                <PhFile :size="24" color="#fff"/>
-                            </div>
-                            <p class="text-sm font-semibold">
-                                {{ file.file_name }}
-                            </p>
-                        </div>
-                        <PhDownloadSimple :size="24" />
-                    </div>
-                </div>
-            </div>
-            <div v-else>
-                No data
-            </div>
-        </div>
+      </div>
     </div>
+    <ButtonView
+      v-if="!showGradationGrowth"
+      :text="'Получить информацию'"
+      :on-click="toggleGradationGrowth"
+      class="my-3"
+    />
+    <ButtonView v-else :text="'Обновить информацию'" class="my-3" />
+
+    <div v-if="showGradationGrowth">
+      <p class="mb-3">Результат запроса</p>
+      <div class="flex justify-between">
+        <p class="text-xs">Последнее обновление:</p>
+        <div class="flex text-xs gap-1">
+          <PhClock :size="16" /> 12:03 <PhCalendarDots :size="16" /> 9.01.2024
+        </div>
+      </div>
+      <div
+        class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center"
+        @click="downloadGradationGrowthFile(gradationActiveData?.file_id)"
+      >
+        <div class="flex gap-3 items-center">
+          <div class="p-1 bg-[#797979] rounded">
+            <PhFile :size="24" color="#fff" />
+          </div>
+          <p class="text-sm font-semibold">
+            {{ gradationActiveData?.file_name }}
+          </p>
+        </div>
+        <PhDownloadSimple :size="24" />
+      </div>
+      <p class="mb-3 text-sm font-semibold">История</p>
+      <div v-if="historyData">
+        <div v-for="(file, index) in historyData.data" key="index">
+          <div class="flex justify-between">
+            <p class="text-[#B8B8B8] text-xs">Дата создания</p>
+            <div class="flex gap-1 text-xs items-center">
+              <PhClock :size="12" />
+              <p>{{ file.time }}</p>
+              <PhCalendarDots :size="16" />
+              <p>{{ file.date }}</p>
+            </div>
+          </div>
+          <div
+            class="bg-[#17181C] p-2 rounded-xl cursor-pointer my-4 flex justify-between items-center"
+            @click="downloadGradationGrowthFile(file.file_id)"
+          >
+            <div class="flex gap-3 items-center">
+              <div class="p-1 bg-[#797979] rounded">
+                <PhFile :size="24" color="#fff" />
+              </div>
+              <p class="text-sm font-semibold">
+                {{ file.file_name }}
+              </p>
+            </div>
+            <PhDownloadSimple :size="24" />
+          </div>
+        </div>
+      </div>
+      <div v-else>No data</div>
+    </div>
+    <Teleport to="body">
+      <div
+        v-if="isNotification"
+        class="modal fixed inset-0 flex items-center justify-center z-50 px-4"
+      >
+        <transition name="modal">
+          <div class="bg-[#222222b3] py-4 px-5 rounded-lg w-full">
+            <div class="flex justify-end">
+              <button
+                @click="isNotification = false"
+                class="bg-[#7474802e] p-2 rounded-full"
+              >
+                <PhX :size="21" />
+              </button>
+            </div>
+            <p class="text-lg py-2 font-semibold text-center">
+              Файл успешно отправлен
+            </p>
+          </div>
+        </transition>
+      </div>
+    </Teleport>
+  </div>
 </template>
