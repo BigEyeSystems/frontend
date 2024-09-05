86d764d4e244d73ac03b84aa23b5638f51267ec9
Kanat <abdsattar02@inbox.ru>
2024-09-05 12:52:36 +0500

    fix(impulse data table)

--- src/components/impulse.vue
+++ src/components/impulse.vue
@@ -125,7 +125,18 @@ const showImpulseData = async () => {
 
     <div v-if="showImpulse">
       <p class="my-4">Выберите импульс для сохранения в историю</p>
-      {{ impulseData?.impulses_history }}
+      <tbody>
+        <tr v-for="(item, index) in impulseData?.impulses_history" :key="index">
+          <td>
+            {{ item.active_name }}
+            <span :class="{ 'positive': item.percent > 0, 'negative': item.percent < 0 }">
+              {{ item.percent.toFixed(2) }}%
+            </span>
+          </td>
+          <td>{{ item.day_percent.toFixed(2) }}%</td>
+          <td>{{ item.date }}</td>
+        </tr>
+      </tbody>
       <div class="mb-4">
         <ticker :detail = impulseData?.impulses_history />
       </div>
