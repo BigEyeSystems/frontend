f9faf1f5b11c3aaf8141572ac568e8321829e70d
Kanat <abdsattar02@inbox.ru>
2024-09-06 13:35:29 +0500

    fix(daisy ui)

--- src/components/fundingData.vue
+++ src/components/fundingData.vue
@@ -293,11 +293,8 @@ const showDate = (timestamp) => {
     </div>
   </div>
   <data v-else>
-    <div class="flex w-52 flex-col gap-4">
-      <div class="skeleton h-32 w-full"></div>
-      <div class="skeleton h-4 w-28"></div>
-      <div class="skeleton h-4 w-full"></div>
-      <div class="skeleton h-4 w-full"></div>
+    <div class="w-full flex justify-center">
+      <p class="font-semibold">Loading...</p>
     </div>
   </data>
 </template>
