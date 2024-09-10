25c7e08668991951bab74db8ce750b38aacdefa3
Kanat <abdsattar02@inbox.ru>
2024-09-10 15:38:36 +0500

    fix(menu item)

--- src/components/menuItem.vue
+++ src/components/menuItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="rounded-2xl p-px bg-gradient-to-r from-zinc-700 from-0% via-zinc-600 via-50% to-zinc-700 to-100% w-1/3 cursor-pointer">
-        <div class="rounded-[calc(1.5rem-10px)] p-1 px-2 bg-[#17181C] h-20 flex flex-col gap-1 justify-center active:opacity-80">
+        <div class="rounded-[calc(1.5rem-10px)] p-1 px-2 bg-[#17181C] h-20 flex flex-col gap-2 justify-center active:opacity-80">
             <slot name="icon"></slot>
             <p class="whitespace-normal text-xs font-semibold">{{ title }}</p>
         </div>
